a7f9e30773fe982a6d4d0d1949bf38e3960633b0
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2023-02-20 16:00:46 +0000

    🧮:👁️:fo1

--- x/wee/u.fish
+++ x/wee/u.fish
@@ -1,1 +1,12 @@
 #!/bin/fish 
+
+# Etherscans
+set eth 2JEANQYC4C9S6PKDFWNGVT2UER24T32D2M
+set emum GEFKFGHEYV286U747PUUCK7ZXNWMKPGC7A
+set ebsc D0ca!@L8g3NTPhccJqiGD0BRhdXUB28g#0Ex%O
+
+# RPCS
+
+set al_go https://eth-goerli.g.alchemy.com/v2/PS0Jslog_yFlroPzNafoC5HqSKUZOiX3
+set al_mu https://polygon-mumbai.g.alchemy.com/v2/fZOoxA6iUz8s3JquDiu4VatGa53OIVNv
+set qui_bsc https://broken-dimensional-paper.bsc-testnet.discover.quiknode.pro/11b885aa0daf11eeb978126a0b33fdf74ff81364/
